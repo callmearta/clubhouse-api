acac75f078092f334b71220674bf7513ec3e51e5
callmearta <salehmoosapour1377@gmail.com>
2021-04-09 14:08:46 +0430

    Fix Invite To existing channel

--- src/api/inviteToExistingChannel.js
+++ src/api/inviteToExistingChannel.js
@@ -1,30 +1,30 @@
-import agent from '../structures/agent'
+import agent from "../structures/agent";
 
 const inviteToExistingChannel = async (profile, opts) => {
-  'use strict'
+	"use strict";
 
-  opts = opts || {}
+	opts = opts || {};
 
-  const response = await agent(
-    '/invite_to_existing_channel',
-    {
-      body: {
-        channel: opts.channel || -1, // NOTE: Channel Id;
-        user_ids: opts.user || -1 // NOTE: User Id;
-      }
-    },
-    profile
-  )
-  const data = await response.json()
+	const response = await agent(
+		"/invite_to_existing_channel",
+		{
+			body: {
+				channel: opts.channel || -1, // NOTE: Channel Id;
+				user_id: opts.user || -1 // NOTE: User Id;
+			}
+		},
+		profile
+	);
+	const data = await response.json();
 
-  return data
-}
+	return data;
+};
 
-export default inviteToExistingChannel
+export default inviteToExistingChannel;
 
 export const specification = {
-  fallback_message: String,
-  fallback_number_hash: Number,
-  notifications_enabled: Boolean,
-  success: Boolean
-}
+	fallback_message: String,
+	fallback_number_hash: Number,
+	notifications_enabled: Boolean,
+	success: Boolean
+};
